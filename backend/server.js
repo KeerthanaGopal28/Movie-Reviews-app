dde307e9984f09f19da01bced92ef74d7b120469
keerthana <keerthanahg28@gmail.com>
2026-08-14 19:11:42 +0530

    addded vercel.json

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',  
+    origin: [
+      'http://localhost:5173',
+      'https://movie-reviews-fullstack-bfwyuvx00-keerthana-hgs-projects.vercel.app'
+    ],  
     credentials: true,
   })
 );
